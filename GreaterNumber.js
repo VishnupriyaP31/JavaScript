173e0e56a2c09ba54295ede2a56b73d0bcea3413
VishnupriyaP31 <113713344+VishnupriyaP31@users.noreply.github.com>
2022-11-26 06:25:45 +0530

    Create GreaterNumber.js

---
+++ GreaterNumber.js
@@ -0,0 +1,12 @@
+const a=10;
+const b=20;
+const c=30;
+if(a>b && a>c){
+    console.log(a+' is Greater');
+}
+else if(b>c){
+    console.log(b+' is Greater');
+}
+else{
+    console.log(c+' is Greater');
+}
